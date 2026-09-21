5bae667cbf8d35f3a162bd81ac570382703af95a
Gajendra Saxena <gajendrasaxena@Gajendras-MacBook-Air.local>
2026-09-21 11:06:24 +0530

    api client code

--- e2e/src/main/java/com/company/framework/api/client/ApiClient.java
+++ e2e/src/main/java/com/company/framework/api/client/ApiClient.java
@@ -1,4 +1,42 @@
 package com.company.framework.api.client;
+
+import io.restassured.response.Response;
+
+
+import static io.restassured.RestAssured.*;
+
+
+import com.company.framework.api.specifications.RequestSpec;
+import com.company.framework.api.specifications.ResponseSpec;
+
 public class ApiClient {
-    
+
+    protected void AppClient() {
+
+    }
+
+    public Response get(String uri) {
+        return given()
+                 .spec(RequestSpec.getRequestSpecification(RequestSpec.headerMap("content-type", "application/json")))
+                .when()
+                .get(uri)
+                .then()
+                .spec(ResponseSpec.getResponseSpecification())
+                .extract().response();
+                
+    }
+
+    public Response post(String url, String uri, Object requestBody) {
+        return given()
+                .baseUri(uri)
+                .spec(RequestSpec.getRequestSpecification(RequestSpec.headerMap("content-type", "application/json")))
+                .body(requestBody)
+                .when()
+                .post(url)
+                .then()
+                .spec(ResponseSpec.getResponseSpecification())
+                .extract()
+                .response();
+
+    }
 }
