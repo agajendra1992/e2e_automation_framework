f8f49a7bcdd87bbc769337b002013708e3ffed00
Gajendra Saxena <gajendrasaxena@Gajendras-MacBook-Air.local>
2026-09-21 16:42:59 +0530

    jenkins issue

--- e2e/src/main/java/com/company/framework/api/client/ApiClient.java
+++ e2e/src/main/java/com/company/framework/api/client/ApiClient.java
@@ -1,61 +1,61 @@
-package com.company.framework.api.client;
-public class ApiClient {
-<<<<<<< Updated upstream
+// package com.company.framework.api.client;
+// public class ApiClient {
+// <<<<<<< Updated upstream
     
-=======
+// =======
 
-    protected void AppClient() {
+//     protected void AppClient() {
 
-    }
+//     }
 
-    public Response get(String uri) {
-        return given()
-                 .spec(RequestSpec.getRequestSpecification(RequestSpec.headerMap("content-type", "application/json")))
-                .when()
-                .get(uri)
-                .then()
-                .spec(ResponseSpec.getResponseSpecification())
-                .extract().response();
-    }          
+//     public Response get(String uri) {
+//         return given()
+//                  .spec(RequestSpec.getRequestSpecification(RequestSpec.headerMap("content-type", "application/json")))
+//                 .when()
+//                 .get(uri)
+//                 .then()
+//                 .spec(ResponseSpec.getResponseSpecification())
+//                 .extract().response();
+//     }          
                 
-    public Response get(String uri,String pathParam) {
-        return given()
-                 .spec(RequestSpec.getRequestSpecification(RequestSpec.headerMap("content-type", "application/json")))
-                 .param("id", pathParam)
-                .when()
-                .get(uri)
-                .then()
-                .spec(ResponseSpec.getResponseSpecification())
-                .extract().response();
+//     public Response get(String uri,String pathParam) {
+//         return given()
+//                  .spec(RequestSpec.getRequestSpecification(RequestSpec.headerMap("content-type", "application/json")))
+//                  .param("id", pathParam)
+//                 .when()
+//                 .get(uri)
+//                 .then()
+//                 .spec(ResponseSpec.getResponseSpecification())
+//                 .extract().response();
                 
     
-    }
+//     }
 
-    public Response post(String uri, Object requestBody) {
-        return given()
-                .spec(RequestSpec.getRequestSpecification(RequestSpec.headerMap("content-type", "application/json")))
-                .body(requestBody)
-                .when()
-                .post(uri)
-                .then()
-                .spec(ResponseSpec.getResponseSpecification())
-                .extract()
-                .response();
+//     public Response post(String uri, Object requestBody) {
+//         return given()
+//                 .spec(RequestSpec.getRequestSpecification(RequestSpec.headerMap("content-type", "application/json")))
+//                 .body(requestBody)
+//                 .when()
+//                 .post(uri)
+//                 .then()
+//                 .spec(ResponseSpec.getResponseSpecification())
+//                 .extract()
+//                 .response();
 
-    }
+//     }
 
-    public Response post(String uri, String pathParam, Object requestBody) {
-        return given()
-                .spec(RequestSpec.getRequestSpecification(RequestSpec.headerMap("content-type", "application/json")))
-                .pathParam("id", pathParam)
-                .body(requestBody)
-                .when()
-                .post(uri)
-                .then()
-                .spec(ResponseSpec.getResponseSpecification())
-                .extract()
-                .response();
+//     public Response post(String uri, String pathParam, Object requestBody) {
+//         return given()
+//                 .spec(RequestSpec.getRequestSpecification(RequestSpec.headerMap("content-type", "application/json")))
+//                 .pathParam("id", pathParam)
+//                 .body(requestBody)
+//                 .when()
+//                 .post(uri)
+//                 .then()
+//                 .spec(ResponseSpec.getResponseSpecification())
+//                 .extract()
+//                 .response();
 
-    }
->>>>>>> Stashed changes
-}
+//     }
+// >>>>>>> Stashed changes
+// }
