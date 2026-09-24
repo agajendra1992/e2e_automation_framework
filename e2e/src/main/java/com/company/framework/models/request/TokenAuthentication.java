778ad416dabfc1cc5ca07164a0e2a76273c4fc98
Gajendra Saxena <gajendrasaxena@Gajendras-MacBook-Air.local>
2026-09-24 18:29:47 +0530

    uodated with image

--- e2e/src/main/java/com/company/framework/models/request/TokenAuthentication.java
+++ e2e/src/main/java/com/company/framework/models/request/TokenAuthentication.java
@@ -1,7 +1,15 @@
 package com.company.framework.models.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TokenAuthentication {
     @JsonProperty("username")
     private String username;
@@ -9,26 +17,4 @@ public class TokenAuthentication {
     @JsonProperty("password")
     private String password;
 
-    public TokenAuthentication(String username, String password){
-        this.username = username;
-        this.password = password;
-    }
-
-    public void setUserName(String username){
-        this.username = username;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-    @JsonProperty("username")
-    public String getUserName(){
-        return username;
-    }
-
-    @JsonProperty("password")
-    public String getPassword(){
-        return password;
-    }
 }
